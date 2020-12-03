2bec6a3e28b292f41d19858066a6f4279a6a8772
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2020-12-03 09:40:29 -0300

    Create firstAwesomeFeature! hehe

---
+++ src/firstAwesomeFeature.js
@@ -0,0 +1,30 @@
+const firstAwesomeFeature = "> This is your first awesome feature! 😎 <";
+
+function runfirstAwesomeFeature() {
+  return new Promise((resolve) => {
+    console.log("> Running your first awesome feature...");
+
+    setTimeout(() => {
+      resolve(firstAwesomeFeature);
+    }, 5000);
+  });
+}
+
+function closefirstAwesomeFeature() {
+  return new Promise((resolve) => {
+    console.log("> Closing your first awesome feature...");
+
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+}
+
+async function start() {
+  await runfirstAwesomeFeature();
+  await closefirstAwesomeFeature();
+
+  console.log("> This ended. Bye bye! 👋");
+}
+
+start();
