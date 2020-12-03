0bda77e5686b9d289082cbd952adde4fcaef5fd8
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2020-12-03 20:45:27 -0300

    Create MAIN FEATURE 😎

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import firstAwesomeFeature from "./firstAwesomeFeature.js";
+import secondAwesomeFeature from "./secondAwesomeFeature.js";
+
+async function main() {
+  await firstAwesomeFeature();
+  await secondAwesomeFeature();
+}
+
+main();
